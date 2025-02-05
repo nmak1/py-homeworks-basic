50024eeca3bc0b9a74bfed363e049a9d4868a754
nmak1 <nmak1@rambler.ru>
2025-02-05 20:57:10 +0300

    Task number 3

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -17,16 +17,52 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        avg_grade = self.calculate_avg_grade()
+        courses_in_progress = ', '.join(self.courses_in_progress)
+        finished_courses = ', '.join(self.finished_courses)
+        return (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:'
+                f' {avg_grade}\nКурсы в процессе изучения: {courses_in_progress}'
+                f'\nЗавершенные курсы: {finished_courses}')
+
+    def calculate_avg_grade(self):
+        if not self.grades:
+            return 0
+        total = sum(sum(grades) for grades in self.grades.values())
+        count = sum(len(grades) for grades in self.grades.values())
+        return round(total / count, 1)
+
+    def __lt__(self, other):
+        return self.calculate_avg_grade() < other.calculate_avg_grade()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def __str__(self):
+        avg_grade = self.calculate_avg_grade()
+        return super().__str__() + f'\nСредняя оценка за лекции: {avg_grade}'
+
+    def calculate_avg_grade(self):
+        if not self.grades:
+            return 0
+        total = sum(sum(grades) for grades in self.grades.values())
+        count = sum(len(grades) for grades in self.grades.values())
+        return round(total / count, 1)
+
+    def __lt__(self, other):
+        return self.calculate_avg_grade() < other.calculate_avg_grade()
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
