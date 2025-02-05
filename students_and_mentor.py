3e8766012685ef1a582ae83798366e8bd57a1836
nmak1 <nmak1@rambler.ru>
2025-02-05 21:36:35 +0300

    Task number 4

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,15 +1,13 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
-        self.finished_courses = []
         self.courses_in_progress = []
+        self.finished_courses = []
         self.grades = {}
 
     def rate_lecture(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -21,9 +19,10 @@ class Student:
         avg_grade = self.calculate_avg_grade()
         courses_in_progress = ', '.join(self.courses_in_progress)
         finished_courses = ', '.join(self.finished_courses)
-        return (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:'
-                f' {avg_grade}\nКурсы в процессе изучения: {courses_in_progress}'
-                f'\nЗавершенные курсы: {finished_courses}')
+        return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {avg_grade}\n'
+                f'Курсы в процессе изучения: {courses_in_progress}\n'
+                f'Завершенные курсы: {finished_courses}')
 
     def calculate_avg_grade(self):
         if not self.grades:
@@ -76,3 +75,71 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+# Создаем экземпляры классов
+lecturer_1 = Lecturer('Ivan', 'Ivanov')
+lecturer_1.courses_attached.append('Python')
+
+lecturer_2 = Lecturer('Sergey', 'Petrov')
+lecturer_2.courses_attached.append('Git')
+
+reviewer_1 = Reviewer('Jon', 'Smit')
+reviewer_1.courses_attached.append('Python')
+
+reviewer_2 = Reviewer('Anna', 'Ivanova')
+reviewer_2.courses_attached.append('Git')
+
+student_1= Student('Roy', 'Eman')
+student_1.courses_in_progress.append('Python')
+student_1.finished_courses.append('Введение в программирование')
+
+student_2 = Student('Ron', 'Wuizli')
+student_2.courses_in_progress.append('Git')
+student_2.finished_courses.append('Введение в программирование')
+
+# Выставляем оценки
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_2.rate_hw(student_2, 'Git', 8)
+reviewer_2.rate_hw(student_2, 'Git', 7)
+
+student_1.rate_lecture(lecturer_1, 'Python', 10)
+student_1.rate_lecture(lecturer_1, 'Python', 9)
+student_2.rate_lecture(lecturer_2, 'Git', 8)
+student_2.rate_lecture(lecturer_2, 'Git', 7)
+
+# Выводим информацию
+print(lecturer_1)
+print(lecturer_2)
+print(reviewer_1)
+print(reviewer_2)
+print(student_1)
+print(student_2)
+
+# Сравниваем студентов и лекторов
+print(student_1 > student_2)
+print(lecturer_1 < lecturer_2)
+
+# Функции для подсчета средней оценки
+def calculate_avg_hw_grade(students, course):
+    total = 0
+    count = 0
+    for student in students:
+        if course in student.grades:
+            total += sum(student.grades[course])
+            count += len(student.grades[course])
+    return round(total / count, 1) if count else 0
+
+def calculate_avg_lecture_grade(lecturers, course):
+    total = 0
+    count = 0
+    for lecturer in lecturers:
+        if course in lecturer.grades:
+            total += sum(lecturer.grades[course])
+            count += len(lecturer.grades[course])
+    return round(total / count, 1) if count else 0
+
+# Подсчет средней оценки за домашние задания и лекции
+print(calculate_avg_hw_grade([student_1, student_2], 'Python'))
+print(calculate_avg_lecture_grade([lecturer_1, lecturer_2], 'Python'))
+
