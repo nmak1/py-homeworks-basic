28ec93c68e76f03879987a2fc6d205b34dff1097
nmak1 <nmak1@rambler.ru>
2025-02-05 20:44:26 +0300

    Task number 2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
